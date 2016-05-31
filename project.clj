bc471fd0910f0b0aebe4b9dd8afe13b15ca633d6
Matt Shanker <me@here.com>
2016-05-30 17:17:45 -0700

    initial import

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject . "0.1.0-SNAPSHOT"
+  :description "Clojure library for the twitch.tv API"
+  :url "https://github.com/hatmatter/clj-twitch-api"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
