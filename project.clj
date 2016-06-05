24d4cf4f9eaa78a6a9f5df5572a62b624bc617b5
Matt Shanker <matt.shanker@gmail.com>
2016-06-05 23:32:00 +0000

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject . "0.1.0-SNAPSHOT"
+(defproject . "0.2.0-SNAPSHOT"
   :description "Clojure library for the twitch.tv API"
   :url "https://github.com/hatmatter/clj-twitch-api"
   :license {:name "Apache License 2.0"
